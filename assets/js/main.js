8fbe13ba701a8c40fe2e9c23b99e29ddaa2e3add
evangtim <evangtimugbaja@hotmail.com>
2021-01-23 20:08:36 +0100

    Page redirect
    Rewritten page  redirect.

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,30 +56,70 @@ sr.reveal('.work__img',{interval: 200});
 /*SCROLL CONTACT*/
 sr.reveal('.contact__input',{interval: 200}); 
 
-/*FORM VALIDATION*/
-function validateForm() {
-    if (document.myForm.fName.value == "") {
-      alert("Name must be filled out");
-      document.myForm.fName.focus ();
-      return false;
-    }
-    if (document.myForm.fTel.value == "") {
-      alert("Tel must be filled out");
-      document.myForm.fTel.focus ();
-      return false;
-    }
-    if (document.myForm.fEmail.value == "") {
-      alert("Email must be filled out");
-      document.myForm.fEmail.focus ();
-      return false;
-    }
-    if (document.myForm.message.value == "") {
-      alert("Message must be filled out");
-      document.myForm.message.focus ();
-      return false;
-    }
-    return (true)
-}
+/*form submit*/
+// We access to the inputs by their id's
+let name = document.getElementById("fname");
+let tel = document.getElementById("tel");
+let email = document.getElementById("email");
+let message = document.getElementById("message");
+
+// Error messages
+let errorElementname = document.getElementById("fname_error");
+let errorElementtel = document.getElementById("tel_error");
+let errorElementemail = document.getElementById("email_error");
+let errorElementmessage = document.getElementById("message_error");
+
+// Form
+let contactForm = document.getElementById("contactform");
+
+// Event listener
+contactForm.addEventListener("submit", function (e) {
+  let messageName = [];
+  let messageTel = [];
+  let messageEmail = [];
+  let messageMessage = [];
+  
+    if (fname.value === "" ||fname.value === null) {
+    messageName.push("* This field is required");
+  }
+
+  if (tel.value === "" || tel.value === null) {
+    messageTel.push("* This field is required");
+  }
+
+  if (email.value === "" || email.value === null) {
+      messageEmail.push("* This field is require");
+  }
+
+  if (message.value === "" || message.value === null) {
+      messageMessage.push("* This field is required")
+  }
+
+  // Statement to shows the errors
+  if (messageName.length || messageTel.length || messageEmail.length || messageMessage.length > 0) {
+    e.preventDefault();
+    errorElementname.innerText = messageName;
+    errorElementtel.innerText = messageTel;
+    errorElementemail.innerText = messageEmail;
+    errorElementmessage.innerText = messageMessage;
+  }
+  
+   // if the values length is filled and it's greater than 2 then redirect to this page
+    if (
+    (fname.value.length > 2,
+    tel.value.length > 2,
+    email.value.length> 2,
+    message.value.length> 2)
+  ) {
+    e.preventDefault();
+    window.location.assign("thanks.html");
+  }
+
+});
+
+
+
+
 
 
 
